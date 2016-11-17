5897d43a349f39cdb8390062e4ad881b39df651e
vanilla-ice <valeraerohin97@mail.ru>
2016-11-17 22:23:41 +0300

    add classnames

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import styles from './App.scss';
 import { connect } from 'react-redux';
 import { increment } from './counter';
+import cn from 'classnames';
 
 class App extends Component {
   render() {
     const { count } = this.props;
 
     return (
-      <div className={styles.app}>
+      <div className={cn(styles.app, styles.rootApp)}>
         <button onClick={ this.props.increment }>тык</button>
         <h1> {count} </h1>
       </div>
