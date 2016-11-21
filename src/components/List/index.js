d310a1b853c2c9b8aafe38dccc4fe53c77e2c9cc
deathyon <maninblacksweater@gmail.com>
2016-11-22 02:47:10 +0300

    Improve checkboxes

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,38 +6,11 @@ import { connect } from 'react-redux';
 import Item from '../Item';
 
 class List extends Component {
-
-  initSliders() {
-    let cats = this.props.list.categoriesMap;
-    const categories = this.props.list.categories;
-
-    let visibleCategories = categories.slice(0, this.props.list.categoriesToShow);
-
-    for(let category of categories) {
-      let index = visibleCategories.indexOf(category);
-
-      category.$.id = Number(category.$.id);
-      category.visible = false;
-
-      if (index !== -1) {
-        category.visible = true;
-        visibleCategories.splice(index, 1);
-      }
-      cats.set(category.$.id, []);
-    }
-
-    const goods = this.props.list.goods;
-
-    for(let item of goods) {
-      cats.get(Number(item.categoryId)).push(item);
-    }
-
-    return cats;
-  }
-
-  renderSlider( categoryList ) {
-
+  renderSliders() {
+    const categoryList = this.props.list.categoriesMap;
+    
     let result = [];
+
     for(let good of categoryList) {
       result.push(<Item slides={good[1]} key={good[0]} />);
     }
@@ -49,7 +22,7 @@ class List extends Component {
     return (
       <div className={styles.List}>
         <div className={global.container}>
-          {this.renderSlider(this.initSliders())}
+          {this.renderSliders()}
         </div>
       </div>
     )
